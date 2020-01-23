736b4503b1d99640cd1639e216834efb76b681d3
Joel Happ <joelhapp7@gmail.com>
2020-01-23 15:57:17 -0700

    Make MakeSound function more efficient

--- index.js
+++ index.js
@@ -10,60 +10,22 @@ for(var i = 0; i < buttonArray.length; i++)
 document.addEventListener("keydown", handleKeyDown);
 
 
-// function SoundBoard(){
-//   this.c4 = new Audio("sounds/waluigi-sounds/waluigi-c4.mp3");
-//   this.d4 = new Audio("sounds/waluigi-sounds/waluigi-d4.mp3");
-//   this.e4 = new Audio("sounds/waluigi-sounds/waluigi-e4.mp3");
-//   this.f4 = new Audio("sounds/waluigi-sounds/waluigi-f4.mp3");
-//   this.g4 = new Audio("sounds/waluigi-sounds/waluigi-g4.mp3");
-//   this.e4 = new Audio("sounds/waluigi-sounds/waluigi-e4.mp3");
-//   this.a4 = new Audio("sounds/waluigi-sounds/waluigi-a4.mp3");
-//   this.c5 = new Audio("sounds/waluigi-sounds/waluigi-c5.mp3");
-//   this.d5 = new Audio("sounds/waluigi-sounds/waluigi-d5.mp3");
-//   this.play = function(note){
-//     switch(note){
-//     case "c4":
-//       this.c4.play();
-//       break;
-//     case "d4":
-//       this.d4.play();
-//       break;
-//     case "e4":
-//       e4.play();
-//       break;
-//     case "f4":
-//       f4.play();
-//       break;
-//     case "g4":
-//       g4.play();
-//       break;
-//     case "a4":
-//       a4.play();
-//       break;
-//     case "b4":
-//       b4.play();
-//       break;
-//     case "c5":
-//       c5.play();
-//       break;
-//     default:
-//       alert("default case activated.");
-//       break;
-//     }
-//   }
-// }
-
 function MusicalNote(htmlNote, note){
+  // MusicalNote object: for holding the actual instance of the note in html,
+  // as well as the musical note as a string.
   this.htmlNote = htmlNote;
   this.note = note;
 }
 
 function handleClick(){
+  // PROMISES: Creates a MusicalNote object based on the data from the click,
+  // and plays that note.
   var myNote = new MusicalNote(this, determineNote(this.innerHTML));
   playNote(myNote);
 }
 
 function handleKeyDown(){
+  // PROMISES: If the key pressed is a valid key, plays a note.
   var checkNote = determineNote(event.key);
   if(checkNote)
   {
@@ -73,15 +35,20 @@ function handleKeyDown(){
 }
 
 function playNote(musicNote){
+  //PROMISES: Updates the display of the note through a CSS class, and calls makeSound.
   musicNote.htmlNote.classList.add("pressed");
   setTimeout(function(){musicNote.htmlNote.classList.remove("pressed")}, 100);
   makeSound(musicNote.note);
 }
 
 function determineNote(key){
+  //PROMISES: Based on the key that was pressed, determines what musical note to play.
   switch (key){
     case "a":
       return "c4";
+      break;
+    case "A":
+      return "c4#";
       break;
     case "s":
       return "d4";
@@ -98,13 +65,18 @@ function determineNote(key){
     case "k":
       return "a4";
       break;
+    case 'K':
+      return "a4#";
+      break;
     case "l":
       return "b4";
       break;
     case "q":
-      return "c5"
+      return "c5";
+    case "Q":
+      return "c5#";
     case "w":
-      return "d5"
+      return "d5";
       break;
     default:
       console.log("Button not found.");
@@ -114,45 +86,50 @@ function determineNote(key){
 }
 
 function makeSound(note){
-  switch (note) {
-    case "c4":
-      var c4 = new Audio("sounds/waluigi-sounds/waluigi-c4.mp3")
-      c4.play();
-      break;
-    case "d4":
-      var d4 = new Audio("sounds/waluigi-sounds/waluigi-d4.mp3");
-      d4.play();
-      break;
-    case "e4":
-      var e4 = new Audio("sounds/waluigi-sounds/waluigi-e4.mp3");
-      e4.play();
-      break;
-    case "f4":
-      var f4 = new Audio("sounds/waluigi-sounds/waluigi-f4.mp3");
-      f4.play();
-      break;
-    case "g4":
-      var g4 = new Audio("sounds/waluigi-sounds/waluigi-g4.mp3");
-      g4.play();
-      break;
-    case "a4":
-      var a4 = new Audio("sounds/waluigi-sounds/waluigi-a4.mp3");
-      a4.play();
-      break;
-    case "b4":
-      var b4 = new Audio("sounds/waluigi-sounds/waluigi-b4.mp3");
-      b4.play();
-      break;
-    case "c5":
-      var c5 = new Audio("sounds/waluigi-sounds/waluigi-c5.mp3");
-      c5.play();
-      break;
-    case "d5":
-      var d5 = new Audio("sounds/waluigi-sounds/waluigi-d5.mp3");
-      d5.play();
-      break;
-    default:
-      console.log("Error: Sound file not found.");
-      break;
-  }
+  //PROMISES: Plays a sound based on the musical note selected.
+  var filePath = "sounds/waluigi-sounds/waluigi-" + note + ".mp3";
+  var sound = new Audio(filePath);
+  sound.play();
 }
+  // Old switch structure
+  //  switch (note) {
+  //   case "c4":
+  //     var c4 = new Audio("sounds/waluigi-sounds/waluigi-c4.mp3")
+  //     c4.play();
+  //     break;
+  //   case "d4":
+  //     var d4 = new Audio("sounds/waluigi-sounds/waluigi-d4.mp3");
+  //     d4.play();
+  //     break;
+  //   case "e4":
+  //     var e4 = new Audio("sounds/waluigi-sounds/waluigi-e4.mp3");
+  //     e4.play();
+  //     break;
+  //   case "f4":
+  //     var f4 = new Audio("sounds/waluigi-sounds/waluigi-f4.mp3");
+  //     f4.play();
+  //     break;
+  //   case "g4":
+  //     var g4 = new Audio("sounds/waluigi-sounds/waluigi-g4.mp3");
+  //     g4.play();
+  //     break;
+  //   case "a4":
+  //     var a4 = new Audio("sounds/waluigi-sounds/waluigi-a4.mp3");
+  //     a4.play();
+  //     break;
+  //   case "b4":
+  //     var b4 = new Audio("sounds/waluigi-sounds/waluigi-b4.mp3");
+  //     b4.play();
+  //     break;
+  //   case "c5":
+  //     var c5 = new Audio("sounds/waluigi-sounds/waluigi-c5.mp3");
+  //     c5.play();
+  //     break;
+  //   case "d5":
+  //     var d5 = new Audio("sounds/waluigi-sounds/waluigi-d5.mp3");
+  //     d5.play();
+  //     break;
+  //   default:
+  //     console.log("Error: Sound file not found.");
+  //     break;
+  // }
